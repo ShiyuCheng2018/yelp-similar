036aa2c307365e254b0dd433596ec7e667593cfe
Shiyu Cheng <starkeyshiyu@gmail.com>
2020-07-26 22:05:25 -0700

    ErrorToast component

---
+++ src/components/ErrorToast/index.jsx
@@ -0,0 +1,31 @@
+import React, {Component} from "react";
+import "./style.css"
+
+class ErrorToast extends Component {
+
+	componentDidMount() {
+		this.timer = setTimeout(()=>{
+			this.props.clearError();
+		}, 3000);
+	}
+
+	componentWillUnmount() {
+		if(this.timer){
+			clearTimeout(this.timer);
+		}
+	}
+
+	render() {
+		const {msg} = this.props;
+
+		return (
+			<div className={"errorToast"}>
+				<div className={"errorToast_text"}>
+					{msg}
+				</div>
+			</div>
+		);
+	}
+}
+
+export default ErrorToast;
--- src/containers/App/index.jsx
+++ src/containers/App/index.jsx
@@ -1,10 +1,34 @@
 import React, {Component} from "react";
+import ErrorToast from "../../components/ErrorToast";
+import {bindActionCreators} from "redux";
+import {connect} from "react-redux";
+import {actions as appActions, getError} from "../../redux/modules/app"
 import "./style.css";
 
-class App extends Component {
-	render() {
-		return <div>App</div>;
+const mapStateToProps = (state, props) =>{
+	return {
+		error: getError(state)
 	}
 }
 
-export default App;
+const mapDispatchToProps = (dispatch) => {
+	return {
+		appActions: bindActionCreators(appActions, dispatch)
+	}
+}
+
+
+class App extends Component {
+
+	render() {
+
+		const {error, appActions: {clearError}} = this.props;
+
+		return <div className={"App"}>
+			{error? <ErrorToast msg={error} clearError={clearError}/>
+				:null}
+		</div>;
+	}
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
